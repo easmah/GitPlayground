d563e1dba722555e328b06fe6b6c138e0f5bbb57
easmah <easmah@me.com>
2023-01-03 15:59:55 +0000

    New Pull Request

--- src/main/java/NewFeature.java
+++ src/main/java/NewFeature.java
@@ -1,5 +1,6 @@
 public class NewFeature {
     public static void main(String[] args) {
         System.out.println("At Asda world");
+        System.out.println("New pull request for testing purposes");
     }
 }
