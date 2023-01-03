fda48ca7b65d9d7a96c7208ecb3b89022cd70f4d
easmah <easmah@me.com>
2023-01-03 13:13:43 +0000

    added but not here

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,10 @@
+
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("We are all welcome");
         System.out.println("We right here and chilling!!! ");
+        System.out.println("We right here!!! but not there");
     }
 }
